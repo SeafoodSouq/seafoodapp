ea4b6279fead6b75f96f1a14ad220384666b1e1f
Milton Espinoza <mespinoza@ip-172-26-6-130.ap-south-1.compute.internal>
2019-04-05 14:44:03 +0000

    fix angular 5 compatibility

--- src/app/app_preloading_strategy.ts
+++ src/app/app_preloading_strategy.ts
@@ -1,12 +1,20 @@
 
 import { PreloadingStrategy, Route } from '@angular/router';
 
-import { Observable, of } from 'rxjs';
+//import { Observable } from 'rxjs';
+import { Observable} from 'rxjs/Rx';
+import { timer } from 'rxjs/observable/timer';
+
+import { of } from 'rxjs/observable/of';
+//import { Observable, of } from 'rxjs';
+//import 'rxjs/add/operator/mergeMap';
+import { mergeMap } from 'rxjs/operators';
+
 
 export class AppPreloadingStrategy implements PreloadingStrategy {
     preload(route: Route, load: Function): Observable<any> {
         const loadRoute = (delay) => delay
-            ? timer(150).pipe(flatMap(_ => load()))
+            ? timer(1000).pipe(mergeMap(_ => load()))
             : load();
         return route.data && route.data.preload
             ? loadRoute(route.data.delay)
