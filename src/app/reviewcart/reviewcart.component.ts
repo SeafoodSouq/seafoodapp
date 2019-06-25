9aa3eb3c103d573745d485782e6cb3cd84523b2d
= <=>
2019-06-25 11:26:08 -0600

    los campos de shipping details no se guardaban en la orden

--- src/app/reviewcart/reviewcart.component.ts
+++ src/app/reviewcart/reviewcart.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import {Location} from '@angular/common';
+import { Location } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reviewcart',
@@ -8,19 +10,39 @@ import {Location} from '@angular/common';
   styleUrls: ['./reviewcart.component.scss']
 })
 export class ReviewcartComponent implements OnInit {
- 
+
   shoppingCartId: any;
-  checked:boolean = true;
+  checked: boolean = true;
+  public myForm:FormGroup;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private _location: Location
-
-
+    private _location: Location,
+    private http: HttpClient
   ) { }
 
- ngOnInit() {
+  ngOnInit() {
+    this.myForm = new FormGroup({
+      firstName1 : new FormControl('', Validators.required),
+      lastName1 : new FormControl('', Validators.required),
+      companyName1 : new FormControl('', Validators.required),
+      street1 : new FormControl('', Validators.required),
+      city1 : new FormControl('', Validators.required),
+      country1 : new FormControl('', Validators.required),
+      sameAsBillingAddress : new FormControl(true, Validators.required),
+      firstName2 : new FormControl('', Validators.nullValidator),
+      lastName2 : new FormControl('', Validators.nullValidator),
+      companyName2 : new FormControl('', Validators.nullValidator),
+      street2 : new FormControl('', Validators.nullValidator),
+      city2 : new FormControl('', Validators.nullValidator),
+      country2 : new FormControl('', Validators.nullValidator),
+    });
+
+    this.myForm.valueChanges.subscribe(it=>{
+      this.checked = it.sameAsBillingAddress;
+    })
+
     this.route.queryParams.subscribe(params => {
       this.shoppingCartId = params['shoppingCartId'];
     });
@@ -29,12 +51,14 @@ export class ReviewcartComponent implements OnInit {
 
 
 
-  goToNextPage(){
-        this.router.navigate(['/checkout'],  {queryParams: {shoppingCartId: this.shoppingCartId}});
-
+  goToNextPage() {
+    let  val = this.myForm.value;
+    this.http.patch('shoppingcart/' + this.shoppingCartId, { shippingDetails: val }).subscribe(() => {
+      this.router.navigate(['/checkout'], { queryParams: { shoppingCartId: this.shoppingCartId } });
+    });
   }
 
-  back(){
+  back() {
     this._location.back();
   }
 
